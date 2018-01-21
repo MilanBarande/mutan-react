432642c1e369431cf55101e3a2dfc83d0acd98fa
Milan <milan.barande@hotmail.fr>
2018-01-21 16:34:00 +0100

    update readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
 
 Documentation for this project coming soon...
+
+More informations about MUTAN can be found [here](https://github.com/Cadene/vqa.pytorch)
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,15 +9,15 @@ const PageNavbar = () => (
       </Navbar.Brand>
     </Navbar.Header>
     <Nav>
-      <NavItem eventKey={1} href="#">
-        Paper
+      <NavItem eventKey={1} href="https://arxiv.org/abs/1705.06676">
+        Read the Paper
       </NavItem>
-      <NavItem eventKey={2} href="#">
-        Github
+      <NavItem eventKey={2} href="https://github.com/Cadene/vqa.pytorch">
+        Github repository
       </NavItem>
       <NavDropdown eventKey={3} title="The authors" id="basic-nav-dropdown">
-        <MenuItem eventKey={3.1}>Rémi Cadène</MenuItem>
-        <MenuItem eventKey={3.2}>Hedi Ben-Younes</MenuItem>
+        <MenuItem eventKey={3.1}>Hedi Ben-Younes</MenuItem>
+        <MenuItem eventKey={3.2}>Rémi Cadène</MenuItem>
         <MenuItem eventKey={3.3}>Matthieu Cord</MenuItem>
         <MenuItem eventKey={3.4}>Nicolas Thome</MenuItem>
       </NavDropdown>
